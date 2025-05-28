0456a81244bf7cc22d2f84913dbba8283b50d50f
hojunJeong <hojun1105@gmail.com>
2025-05-29 03:25:47 +0900

    Initial commit

---
+++ src/main/kotlin/com/demo/univchatbot/UnivChatbotApplication.kt
@@ -0,0 +1,11 @@
+package com.demo.univchatbot
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UnivChatbotApplication
+
+fun main(args: Array<String>) {
+    runApplication<UnivChatbotApplication>(*args)
+}
---
+++ src/test/kotlin/com/demo/univchatbot/UnivChatbotApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.demo.univchatbot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UnivChatbotApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
